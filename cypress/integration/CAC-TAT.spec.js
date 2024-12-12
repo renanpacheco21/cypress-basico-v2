60e2b4c23276f9c4889c0d3c1432f71d5bcea336
@renan.matos <renanpacheco.delta@gmail.com>
2024-12-11 22:31:42 -0300

    Aula 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,32 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+describe("Central de Atendimento ao Cliente TAT", function () {
+  beforeEach(function () {
+    cy.visit("./src/index.html");
+  });
+
+  it("verifica o título da aplicação", function () {
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+
+  it("preenche campos obrigatórios e envia o formulário", function () {
+    const longText =
+      "teste abc 1234567890 teste abc 0 teste abc 1234567890 teste abc 1234567890 teste abc 1234567890 teste abc 1234567890 ";
+
+    cy.get("#firstName").type("Renan");
+    cy.get("#lastName").type("Pacheco de Matos");
+    cy.get("#email").type("teste@teste.com");
+    cy.get("#open-text-area").type(longText, { delay: 0 });
+    cy.get("button[type='submit']").click();
+    cy.get(".success").should("be.visible");
+  });
+
+  it.only("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", function () {
+    cy.get("#firstName").type("Renan");
+    cy.get("#lastName").type("Pacheco de Matos");
+    cy.get("#email").type("teste-teste.com");
+    cy.get("#open-text-area").type("teste", { delay: 0 });
+    cy.get("button[type='submit']").click();
+    cy.get(".error").should("be.visible");
+  });
+});
